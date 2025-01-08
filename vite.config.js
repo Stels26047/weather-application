cda45c5693856493a62fb5cf8feda67cfb196e53
StelsMaster <58134253+Stels26047@users.noreply.github.com>
2025-01-08 19:42:18 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: 'weather-application/dist'
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
